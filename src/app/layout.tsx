4e36680822fa2a9a064121f36f5304876e59958f
Abdul-WriteCodes <145736001+Abdul-WriteCodes@users.noreply.github.com>
2026-08-05 23:32:23 +0100

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const viewport: Viewport = {
   maximumScale: 1,
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang="en"
